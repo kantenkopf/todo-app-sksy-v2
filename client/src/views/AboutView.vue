af20b087e3ff7b8038bd7379d2fab8f3f5dfe2a2
Maurice Riebling <m.riebling@outlook.de>
2022-05-13 18:58:28 +0200

    Refactor to fit style (2 spaces for indentation)

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container text-center">
-        <div class="row mb-5">
-            <p>Created by:</p>
-            <p>Maurice Fabrice Riebling & Oliver Morthen Fuhrmann</p>
-        </div>
+  <div class="container text-center">
+    <div class="row mb-5">
+      <p>Created by:</p>
+      <p>Maurice Fabrice Riebling & Oliver Morthen Fuhrmann</p>
     </div>
+  </div>
 </template>
