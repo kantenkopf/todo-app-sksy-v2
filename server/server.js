d068fa15cbe8feb62748355c33c1e23e957effff
Maurice Riebling <m.riebling@outlook.de>
2022-05-13 11:25:03 +0200

    Server now preserves routes from vue-router

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const history = require("connect-history-api-fallback");
 
 require("dotenv").config();
 require("./helpers/mongodb.helper");
@@ -8,6 +9,13 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
+app.use(history({
+  //uncomment to expose and directly display api content in browser
+  /*
+  rewrites: [
+    { from: /\/api/, to: '/api'}
+  ]*/
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
