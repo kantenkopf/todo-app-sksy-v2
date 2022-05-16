1a2c72719abff022ca3f57c31fd716dcb2101aa1
Maurice Riebling <m.riebling@outlook.de>
2022-05-16 22:10:02 +0200

    Added form validation to new todos

--- client/src/views/NewView.vue
+++ client/src/views/NewView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <BaseForm @submit.prevent @save="addTodo">
+    <BaseForm @submit.prevent :isValid="isValid" @save="addTodo">
       <BaseFormInput :labelFor="'task-description'" :helpId="'description-help'">
         <template #label>Task Description</template>
         <input class="form-control" type="text" name="task-description" id="task-description" placeholder="My next task..." maxlength="160" v-model="description">
@@ -9,7 +9,10 @@
 
       <BaseFormInput :labelFor="'task-description'" :helpId="'description-help'">
         <template #label>Task Description</template>
-        <input class="form-control" type="date" name="due-date" id="due-date" v-model="date">
+        <input class="form-control" :class="{ 'is-invalid': dueDate < `${new Date().toISOString().split('T')[0]}T00:00:00.000Z`}" type="date" name="due-date" id="due-date" v-model="date">
+        <div class="invalid-feedback">
+          Date can't be in the past.
+        </div>
         <template #help>What do you need to get done?</template>
       </BaseFormInput>
     </BaseForm>
@@ -26,7 +29,7 @@ export default {
     data() {
       return {
         description: "",
-        dueDate: new Date().toISOString().split('T')[0]+"T00:00:00.000Z",
+        dueDate: `${new Date().toISOString().split('T')[0]}T00:00:00.000Z`,
         date: new Date().toISOString().split('T')[0],
       }
     },
@@ -36,9 +39,14 @@ export default {
         this.$router.push('/');
       }
     },
+    computed: {
+      isValid() {
+        return this.dueDate >= `${new Date().toISOString().split('T')[0]}T00:00:00.000Z`;
+      }
+    },
     watch: {
       date() {
-        this.dueDate = this.date + "T00:00:00.000Z";
+        this.dueDate = `${this.date}T00:00:00.000Z`;
       },
     }
 }
